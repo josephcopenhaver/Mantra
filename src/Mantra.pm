21f0a9a0d60f430a0e07657d7ccd0eb553f519ad
Joseph Copenhaver <joseph.copenhaver@gmail.com>
2014-09-13 04:24:45 -0500

    Playing nice with my old windows version of perl...

--- src/Mantra.pm
+++ src/Mantra.pm
@@ -8,14 +8,16 @@ our @EXPORT_OK = ();
 use Mantra::Framework;
 use Mantra::Constants;
 
-my $packagePrefix = "Mantra::Generator";
+use constant {
+	PACKAGE_PREFIX => "Mantra::Generator::"
+};
 
 ###
 # Alloc new file generator
 sub new {
 	my ($self, $class, $generatorName) = ({}, @_);
 	die unless ($generatorName !~ /[\r\n]/ && $generatorName =~ /^[a-zA-Z0-9_\:]+$/);
-	$generatorName = "$packagePrefix::$generatorName";
+	$generatorName = PACKAGE_PREFIX . $generatorName;
 	my ($newCallFinished, $generator) = (undef, undef);
 	eval("use $generatorName;\n\$generator = $generatorName\->new();\n\$newCallFinished = 1;");
 	die("Failed to find code generation class named \"$generatorName\"\n$@ $!") if ($@ || (!(defined $generator) && !$newCallFinished));
